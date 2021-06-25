81e5fce774256b773000ce8c3d6888d24db308b2
Why Do We Wonder <37032797+WhyDoWeWonder@users.noreply.github.com>
2021-06-25 17:05:41 -0400

    Post-merge cleanup

--- cogs/stats.py
+++ cogs/stats.py
@@ -47,19 +47,15 @@ class Stats(commands.Cog):
                     async with cs.get('https://tgmapi.cylonemc.net/mc/player/' + requested_user + flags, ) as r:
                         res = await r.json()
                         # If the specified user, whether by username, UUID, or playerID does not exist
-
                         # We inform the user
                         if 'notFound' in res and res['notFound']:
                             # In the future, we should add something here to look up a player's name on Mojang's servers
-
                             # Or on NameMC to check if a username has been changed, if so tell the user that the player
-
                             # Who's stats they're looking up may have changed their name and deal with it appropiately
                             embed_var = discord.Embed(
                                 title="The user you specified is not in Cylone's database, please check your spelling.",
                                 color=0xFF0000)
-                                await ctx.send(embed=embedVar)
-
+                            await ctx.send(embed=embed_var)
                             return
                         #######
                         mc_name = res['user']['name']
@@ -128,36 +124,32 @@ class Stats(commands.Cog):
                         page1.add_field(name="Last Online", value=human(ms / 1000.0), inline=True)
                         page1.add_field(name="Join Date", value=human(ms2 / 1000.0), inline=True)
                         page1.timestamp = datetime.datetime.utcnow()
-                            page1.set_footer(text='Bot Created by ksndq and LordofLightning', icon_url="https://cdn.discordapp.com/icons/754890606173487154/a_d0357357c6115502b46b996be1fb32d6.webp?size=64")
-                                         icon_url="https://cdn.discordapp.com/avatars/431703739913732097/013868d08ceb35bf90fb568bfbd1e854.png?size=64")
+                        page1.set_footer(text='Bot Created by ksndq and LordofLightning', icon_url="https://cdn.discordapp.com/icons/754890606173487154/a_d0357357c6115502b46b996be1fb32d6.webp?size=64")
                         page1.set_image(url='https://crafatar.com/renders/head/' + skin)
                     ################################################################
-                    async with aiohttp.ClientSession() as cs:
-                        async with cs.get('https://tgmapi.cylonemc.net/mc/match/latest/' + mc_name, ) as r:
-                            res = await r.json()
-                            ms3 = res[0]['match']['startedDate']
-                            i = 0
-                            page2 = discord.Embed(title="", color=0xbc2a82)
-                            page2.set_author(name=mc_name + " Latest Match Stats 2/2")
-                            page2.add_field(name="<a:redblue:853636359108558898> Winning Team",
-                                            value=(res[0]["match"]["winningTeam"].capitalize()), inline=False)
-                            page2.add_field(name="<a:match:854808917024309328> Match Size", value=(res[0]["matchSize"]),
-                                            inline=False)
-                            page2.add_field(name="<:maps:853637839064924170> Map", value=(res[0]["loadedMap"]["name"]),
-                                            inline=False)
-                            page2.add_field(name="<a:clock:854800563857784872> Time elapsed",
-                                            value=(res[0]["timeElapsed"]), inline=True)
-                            page2.add_field(name="Started", value=(
-                                datetime.datetime.fromtimestamp(ms3 / 1000.0).strftime('%m-%d • %H:%M:%S')),
-                                            inline=True)
-                            page2.timestamp = datetime.datetime.utcnow()
-                                page2.set_footer(text='Bot Created by ksndq and LordofLightning', icon_url="https://cdn.discordapp.com/icons/754890606173487154/a_d0357357c6115502b46b996be1fb32d6.webp?size=64")
-                                             icon_url="https://cdn.discordapp.com/avatars/431703739913732097/013868d08ceb35bf90fb568bfbd1e854.png?size=64")
-                            page2.set_image(url='https://crafatar.com/renders/head/' + skin)
+                    async with cs.get('https://tgmapi.cylonemc.net/mc/match/latest/' + mc_name, ) as r:
+                        res = await r.json()
+                        ms3 = res[0]['match']['startedDate']
+                        i = 0
+                        page2 = discord.Embed(title="", color=0xbc2a82)
+                        page2.set_author(name=mc_name + " Latest Match Stats 2/2")
+                        page2.add_field(name="<a:redblue:853636359108558898> Winning Team",
+                                        value=(res[0]["match"]["winningTeam"].capitalize()), inline=False)
+                        page2.add_field(name="<a:match:854808917024309328> Match Size", value=(res[0]["matchSize"]),
+                                        inline=False)
+                        page2.add_field(name="<:maps:853637839064924170> Map", value=(res[0]["loadedMap"]["name"]),
+                                        inline=False)
+                        page2.add_field(name="<a:clock:854800563857784872> Time elapsed",
+                                        value=(res[0]["timeElapsed"]), inline=True)
+                        page2.add_field(name="Started", value=(
+                            datetime.datetime.fromtimestamp(ms3 / 1000.0).strftime('%m-%d • %H:%M:%S')),
+                                        inline=True)
+                        page2.timestamp = datetime.datetime.utcnow()
+                        page2.set_footer(text='Bot Created by ksndq and LordofLightning', icon_url="https://cdn.discordapp.com/icons/754890606173487154/a_d0357357c6115502b46b996be1fb32d6.webp?size=64")
+                        page2.set_image(url='https://crafatar.com/renders/head/' + skin)
         else:
             embed_var = discord.Embed(title="You can't use that here!", color=0xFF0000)
             await ctx.send(embed=embed_var)
-
             pass
 
         pages = [page1, page2]

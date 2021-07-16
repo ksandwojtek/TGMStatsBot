1901ab5a111d9c0e07ae8913f03a505a6a822f35
Why Do We Wonder <37032797+WhyDoWeWonder@users.noreply.github.com>
2021-07-16 15:55:32 -0400

    Pep8 compliance with cat command code.

--- cogs/cat.py
+++ cogs/cat.py
@@ -13,13 +13,13 @@ async def process_cat_command(self, ctx):
     if ctx.channel.id in self.global_variables.config['bot']['channels']:
         connector = ProxyConnectorWrapper().connector
         async with aiohttp.ClientSession(connector=connector) as session:
-            catPicture = requests.get('http://thecatapi.com/api/images/get.php')
-            if catPicture.status_code == 200:
-                catPicture = catPicture.url
+            cat_picture = requests.get('http://thecatapi.com/api/images/get.php')
+            if cat_picture.status_code == 200:
+                cat_picture = cat_picture.url
             colors = [0xFF0000, 0xFF7F00, 0xFFFF00, 0x00FF00, 0x0000FF, 0x2E2B5F, 0x8B00FF]
-            embed_Var = discord.Embed(title="Here is a cat! 🐈", color=random.choice(colors))
-            embed_Var.set_image(url=catPicture)
-            await ctx.send(embed=embed_Var)
+            embed_var = discord.Embed(title="Here is a cat! 🐈", color=random.choice(colors))
+            embed_var.set_image(url=cat_picture)
+            await ctx.send(embed=embed_var)
             pass
 
 
